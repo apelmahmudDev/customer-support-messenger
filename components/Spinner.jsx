19bb29ddb89ecd1ac002928e5ac8f4b4451a7340
Apel Mahmud <apel.techvill@gmail.com>
2023-09-11 15:01:20 +0600

    [feat] :tada: Add a spinner

---
+++ components/Spinner.jsx
@@ -0,0 +1,52 @@
+const Spinner = () => {
+	return (
+		<svg
+			width="20"
+			height="20"
+			viewBox="0 0  24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<g className="spinner_Wezc">
+				<circle fill="#ffcf4b" cx="12" cy="2.5" r="1.5" opacity=".14" />
+				<circle
+					fill="#ffcf4b"
+					cx="16.75"
+					cy="3.77"
+					r="1.5"
+					opacity=".29"
+				/>
+				<circle
+					fill="#ffcf4b"
+					cx="20.23"
+					cy="7.25"
+					r="1.5"
+					opacity=".43"
+				/>
+				<circle
+					fill="#ffcf4b"
+					cx="21.50"
+					cy="12.00"
+					r="1.5"
+					opacity=".57"
+				/>
+				<circle
+					fill="#ffcf4b"
+					cx="20.23"
+					cy="16.75"
+					r="1.5"
+					opacity=".71"
+				/>
+				<circle
+					fill="#ffcf4b"
+					cx="16.75"
+					cy="20.23"
+					r="1.5"
+					opacity=".86"
+				/>
+				<circle fill="#ffcf4b" cx="12" cy="21.5" r="1.5" />
+			</g>
+		</svg>
+	);
+};
+
+export default Spinner;
