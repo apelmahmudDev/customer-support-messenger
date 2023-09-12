f7bcf7d4c7a1d73875fb8482a7fe2ce3028237e0
Apel Mahmud <apel.techvill@gmail.com>
2023-09-12 14:29:36 +0600

    [feat] [refactor] :lock: :wrench: Configure next.config.js & keep secure

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	output: "export",
-	assetPrefix: `/${process.env.ASSET_PREFIX}`,
+	output: process.env.NODE_ENV === "production" ? "export" : "",
+	assetPrefix:
+		process.env.NODE_ENV === "production"
+			? `/${process.env.ASSET_PREFIX}`
+			: "",
+	images: {
+		unoptimized: process.env.NODE_ENV === "production" ? true : false,
+	},
 };
 
 module.exports = nextConfig;
