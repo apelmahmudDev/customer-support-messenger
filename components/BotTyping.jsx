71e61d8db50e7c6a32399d6831cd2d63343eff21
Apel Mahmud <apel.techvill@gmail.com>
2023-09-14 10:34:12 +0600

    [refactor] [style] :recycle: :lipstick: Typing loader

--- components/BotTyping.jsx
+++ components/BotTyping.jsx
@@ -3,5 +3,16 @@ import { useSelector } from "react-redux";
 
 export const BotTyping = () => {
 	const { isBotTyping } = useSelector((state) => state.ui);
-	if (isBotTyping) return <div className="my-2">Typing...</div>;
+	if (isBotTyping)
+		return (
+			<div className="my-2">
+				<div class="py-3 px-4 inline-block rounded-lg">
+					<div class="bot-typing__wrap flex items-center h-[17px]">
+						<div class="circle"></div>
+						<div class="circle"></div>
+						<div class="circle"></div>
+					</div>
+				</div>
+			</div>
+		);
 };
