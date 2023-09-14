db3dcb01f5cc68bb492e1adea713386fce263066
Apel Mahmud <apel.techvill@gmail.com>
2023-09-14 16:33:51 +0600

    [fix] :bug: class to className

--- components/BotTyping.jsx
+++ components/BotTyping.jsx
@@ -6,11 +6,11 @@ export const BotTyping = () => {
 	if (isBotTyping)
 		return (
 			<div className="my-2">
-				<div class="py-3 px-4 inline-block rounded-lg">
-					<div class="bot-typing__wrap flex items-center h-[17px]">
-						<div class="circle"></div>
-						<div class="circle"></div>
-						<div class="circle"></div>
+				<div className="py-3 px-4 inline-block rounded-lg">
+					<div className="bot-typing__wrap flex items-center h-[17px]">
+						<div className="circle"></div>
+						<div className="circle"></div>
+						<div className="circle"></div>
 					</div>
 				</div>
 			</div>
