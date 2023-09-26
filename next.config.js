4599eea90ce4bb77585f6eeebae2991d99f4859b
Apel Mahmud <apel.techvill@gmail.com>
2023-09-26 16:09:04 +0600

    [refactor] :recycle: Modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ["staging.artifism.techvill.net", "lh3.googleusercontent.com"],
+	},
+};
 
 module.exports = nextConfig;
