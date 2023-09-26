9f73d43541ee229e098009182477ac03c5c00a41
Apel Mahmud <apel.techvill@gmail.com>
2023-09-26 16:21:37 +0600

    [refactor] :recycle: Modify next.config.js file for laravel setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+	output: process.env.NODE_ENV === "production" ? "export" : "",
+	assetPrefix:
+		process.env.NODE_ENV === "production"
+			? `/${process.env.ASSET_PREFIX}`
+			: "",
 	images: {
+		unoptimized: process.env.NODE_ENV === "production" ? true : false,
 		domains: ["staging.artifism.techvill.net", "lh3.googleusercontent.com"],
 	},
 };
