0f349b184bc675a04af47905865951272c43e4f9
Apel Mahmud <apel.techvill@gmail.com>
2023-09-30 13:41:59 +0600

    [refactor] :recycle: configure next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	output: process.env.NODE_ENV === "production" ? "export" : "",
-	assetPrefix:
-		process.env.NODE_ENV === "production"
-			? `/${process.env.ASSET_PREFIX}`
-			: "",
 	images: {
-		unoptimized: process.env.NODE_ENV === "production" ? true : false,
 		domains: ["staging.artifism.techvill.net", "lh3.googleusercontent.com"],
 	},
-};
+  }
 
 module.exports = nextConfig;
